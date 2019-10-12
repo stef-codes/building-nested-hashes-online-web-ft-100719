e6df60706fb5051941543e5e0da42b9173b48ac7
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-12 03:34:46 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -20,7 +20,14 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        {:name => "", 
+         :age => "17",
+         :attitude => "worried"
+        },
+        {:name => "Mercutio", 
+         :age => "18",
+         :attitude => "hot-headed"
+        } 
       ]
    }
 }
